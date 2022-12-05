59c966add50f29292dc34185cbd431a7178d2698
Erlend Eriksen <erlend.eriksen@eelume.com>
2022-12-05 20:03:08 +0100

    day 5 part 2

--- 5/main.py
+++ 5/main.py
@@ -8,10 +8,15 @@ lines = f.read().split('\n')
 stacks = [[stack][0] for stack in [[line[i] for line in lines if len(line) > i and line[i].isupper()] for i in range(50)] if stack]
 instructions = [[int(word) for word in line.split(' ') if word.isnumeric()] for line in lines if len(line) > 1 and line[0].islower()]
 
+## part 1
+#for instruction in instructions:
+#    for i in range(instruction[0]):
+#        stacks[instruction[2]-1].insert(0, stacks[instruction[1]-1].pop(0))
+
+# part 2
 for instruction in instructions:
-    for i in range(instruction[0]):
-        stacks[instruction[2]-1].insert(0, stacks[instruction[1]-1].pop(0))
+    stacks[instruction[2]-1][0:0] = stacks[instruction[1]-1][:instruction[0]]
+    del stacks[instruction[1]-1][:instruction[0]]
 
 tops = [stack[0] for stack in stacks]
-
 print("".join(tops))
